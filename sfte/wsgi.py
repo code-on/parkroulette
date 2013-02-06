392ed01a33689f61c802d7c5eb7a4ca03c145b9b
kosmos342 <e.bespaliy@code-on.be>
2013-02-06 14:37:37 +0200

    fixed wsgi.py

--- sfte/wsgi.py
+++ sfte/wsgi.py
@@ -1,14 +1,18 @@
-"""
-WSGI config for sfte project.
+#!/usr/bin/env python
+import os, sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+def rel(*x):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
+sys.path.insert(0, rel('..', 'lib'))
+sys.path.insert(0, rel('..', 'sfte'))
 
-import os
+# Developed and tested on django 1.5
+import django
+assert django.VERSION[0:2] == (1, 5)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
