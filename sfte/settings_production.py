0b4c5fbef51f873cf9cf8c0d5ae5f1de96d4828c
Vitaliy <ppr.vitaly@gmail.com>
2013-04-10 12:09:08 +0300

    new db

--- sfte/settings_production.py
+++ sfte/settings_production.py
@@ -1,1 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'parkroulette2012',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+    }
+}
+
 ENABLE_ANALYTICS = True
