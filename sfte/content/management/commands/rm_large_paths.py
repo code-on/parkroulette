a85861bc361ce15a3a4328d004afca7aff37e8ef
Vitaliy <ppr.vitaly@gmail.com>
2013-05-27 18:06:15 +0200

    memory usage optimisation

--- sfte/content/management/commands/rm_large_paths.py
+++ sfte/content/management/commands/rm_large_paths.py
@@ -2,14 +2,20 @@ from content.models import Path
 from django.core.management import BaseCommand
 
 
+CHUNK_SIZE = 1000
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print 'Total', Path.objects.count()
+        total = Path.objects.count()
+        print 'Total', total
         removed = 0
-        for p in Path.objects.all().iterator():
-            if len(p.path) > 250:
-                removed += 1
-                print removed
-                p.delete()
+
+        for i in range(0, total, CHUNK_SIZE):
+            print '---- %d' % i
+            for p in Path.objects.all()[i:i+CHUNK_SIZE].iterator():
+                if len(p.path) > 250:
+                    removed += 1
+                    p.delete()
+                print '%d (%d)' % (p.path_id, removed)
